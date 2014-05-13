d83eab16320831f5916a582abb999d552d185693
sebastian.bogado <sebastian.bogado@fdvsolutions.com>
2014-05-13 07:27:24 -0300

    Logger setters, y funcionalidades de logueo: debug, info, warn, error y fatal

--- src/main/java/com/fiuba/tdd/logger/Logger.java
+++ src/main/java/com/fiuba/tdd/logger/Logger.java
@@ -1,28 +1,73 @@
 package com.fiuba.tdd.logger;
 
 import com.fiuba.tdd.logger.internal.LoggerInvoker;
+import com.fiuba.tdd.logger.internal.MessageFormatter;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Logger {
 
+    private Level level;
+    private String format;
+    private String separator;
+    private List<Appendable> outputs;
+
     public enum Level {DEBUG, INFO, WARN, ERROR, FATAL, OFF}
 
-    public void setLevel(Level level){}
-
-    public void setFormat(final String format){}
-
-    public void registerAppender(Appendable writer){}
-
-    public void debug(String msg){
-
-
-        LoggerInvoker invoker = new LoggerInvoker(Thread.currentThread().getStackTrace()[1]);
+    public void setLevel(Level level){
+        this.level = level;
     }
 
-    public void info(String msg){}
+    public void setFormat(final String format){
+        this.format = format;
+    }
 
-    public void warn(String msg){}
+    public void setSeparator(final String separator){
+        this.separator = separator;
+    }
 
-    public void error(String msg){}
+    public void registerAppender(Appendable writer){
+        outputs.add(writer);
+    }
 
-    public void fatal(String msg){}
+    public void debug(String msg){
+        log(msg, Level.DEBUG);
+    }
+
+    public void info(String msg){
+        log(msg, Level.INFO);
+    }
+
+    public void warn(String msg){
+        log(msg, Level.WARN);
+    }
+
+    public void error(String msg){
+        log(msg, Level.ERROR);
+    }
+
+    public void fatal(String msg){
+        log(msg, Level.FATAL);
+    }
+
+    private void log(String msg, Level level) {
+        if (level.ordinal() < this.level.ordinal())
+            return;
+
+        LoggerInvoker invoker = new LoggerInvoker(Thread.currentThread().getStackTrace()[2]);  // Magic number 2!
+        MessageFormatter messageFormatter = new MessageFormatter(this.format, this.separator, level);
+
+        String finalMsg = messageFormatter.formatMessage(invoker, msg);
+
+        // TODO discuss how to handle IOException
+        for ( Appendable output : outputs ) {
+            try {
+                output.append(finalMsg);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+
+    }
 }
