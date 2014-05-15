ef478fcc39fd79734f30dead7f86ac055e34b8db
sebastian.bogado <sebastian.bogado@fdvsolutions.com>
2014-05-15 09:35:14 -0300

    Comentarios en InvalidArgumentException, aunque con el nombre alcance...

--- src/main/java/com/fiuba/tdd/logger/internal/InvalidArgumentException.java
+++ src/main/java/com/fiuba/tdd/logger/internal/InvalidArgumentException.java
@@ -1,5 +1,8 @@
 package com.fiuba.tdd.logger.internal;
 
+/**
+ * Excepción lanzada cuando los argumentos son inválidos.
+ */
 public class InvalidArgumentException extends Exception {
 
     public InvalidArgumentException(final String msg){
